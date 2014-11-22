421743f49627ec9666c1c86722d76763a46e8eaa
Hansen Makangiras <hansenmakangiras@gmail.com>
2014-11-21 16:14:19 -0800

    Added Changes

--- app/views/site/layouts/default.blade.php
+++ app/views/site/layouts/default.blade.php
@@ -129,20 +129,20 @@
 				<div class="row">
 					<div class="col-md-4">
 						<h3>Get involved</h3>
-						<p>{{ Config::get('site.name') }} is hosted on <a href="https://github.com/yajra/laravel-admin-template" target="_blank" rel="external nofollow">GitHub</a> and open for everyone to contribute. Please give us some feedback and join the development!</p>
+						<p>{{ Config::get('site.name') }} is hosted on <a href="https://github.com/hansenmakangiras/sim-pmk" target="_blank" rel="external nofollow">GitHub</a> and open for everyone to contribute. Please give us some feedback and join the development!</p>
 					</div>
 					<div class="col-md-4">
 						<h3>Contribute</h3>
-						<p>You want to help us and participate in the development or the documentation? Just fork {{ Config::get('site.name') }} on <a href="https://github.com/yajra/laravel-admin-template" target="_blank" rel="external nofollow">GitHub</a> and send us a pull request.</p>
+						<p>You want to help us and participate in the development or the documentation? Just fork {{ Config::get('site.name') }} on <a href="https://github.com/hansenmakangiras/sim-pmk" target="_blank" rel="external nofollow">GitHub</a> and send us a pull request.</p>
 					</div>
 					<div class="col-md-4">
 						<h3>Found a bug?</h3>
-						<p>Open a <a href="https://github.com/yajra/laravel-admin-template/issues" target="_blank" rel="external nofollow">new issue</a> on GitHub. Please search for existing issues first and make sure to include all relevant information.</p>
+						<p>Open a <a href="https://github.com/hansenmakangiras/sim-pmk/issues" target="_blank" rel="external nofollow">new issue</a> on GitHub. Please search for existing issues first and make sure to include all relevant information.</p>
 					</div>
 				</div>
 				<hr class="dashed">
 				<div class="copyright clearfix">
-					<p><b>{{ Config::get('site.name') }}</b>&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://github.com/yajra/laravel-admin-template">Getting Started</a>&nbsp;•&nbsp;<a href="https://github.com/yajra/laravel-admin-template">Documentation</a>
+					<p><b>{{ Config::get('site.name') }}</b>&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://github.com/hansenmakangiras/sim-pmk">Getting Started</a>&nbsp;•&nbsp;<a href="https://github.com/hansenmakangiras/sim-pmk">Documentation</a>
 						<p>© 2014 <a href="{{ url('/') }}" target="_blank">{{ Config::get('site.name') }}</a>. All rights reserved. &nbsp;&nbsp;Code licensed under <a href="http://opensource.org/licenses/mit-license.html" target="_blank" rel="external nofollow">MIT License</a>, documentation under <a href="http://creativecommons.org/licenses/by/3.0/" rel="external nofollow">CC BY 3.0</a>.</p>
 					</div>
 				</div>
